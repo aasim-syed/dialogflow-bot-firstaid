5d8f192248c02ea884ecf20a2d5844508b4e32ed
aasim-syed <70385414+aasim-syed@users.noreply.github.com>
2021-10-20 05:48:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 const config = require("./server/config/keys");
-// const mongoose = require("mongoose");
-// mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => console.log('MongoDB Connected...'))
-//   .catch(err => console.log(err));
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -15,13 +12,12 @@ app.use(bodyParser.json());
 
 app.use('/api/dialogflow', require('./server/routes/dialogflow'));
 
-// Serve static assets if in production
+
 if (process.env.NODE_ENV === "production") {
 
-  // Set static folder
+
   app.use(express.static("client/build"));
 
-  // index.html for all page routes
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
